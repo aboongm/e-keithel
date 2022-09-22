94e36cade62c9793d4d8b7298ed21ddd54b4cb0d
aboongm <aboongm@yahoo.com>
2022-09-22 18:40:12 +0530

    Created the components skeleton

--- e-keithel-frontend/src/App.js
+++ e-keithel-frontend/src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import Unauthorized from './components/Unauthorized';
+import Linkpage from './components/Linkpage';
+import Checkout from './components/Checkout';
+import Profile from './components/Profile';
+import History from './components/History';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello</h1>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="home" element={<Home />} />
+          <Route path="products" element={<Product />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="linkpage" element={<Linkpage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          {/* we want to protect these routes */}
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="history" element={<History />} />
+
+          {/* catch all */}
+          {/* <Route path="*" element={<Missing />} /> */}
+        </Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ e-keithel-frontend/src/components/Linkpage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Linkpage = () => {
+  return <div>Linkpage</div>;
+};
+
+export default Linkpage;
