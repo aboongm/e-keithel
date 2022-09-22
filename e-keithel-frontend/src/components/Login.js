1cb02d431c5ab91343fcf35833a99b4e28832003
aboongm <aboongm@yahoo.com>
2022-09-22 19:43:57 +0530

    Update login

--- e-keithel-frontend/src/components/Login.js
+++ e-keithel-frontend/src/components/Login.js
@@ -27,7 +27,39 @@ const Login = () => {
     setErrMsg('');
   }, [user, pwd]);
 
-  const handleSubmit = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          header: { 'Content-Type': 'application/json' },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data));
+      // console.log(JSON.stringify(respons));
+      const roles = response?.data?.roles;
+
+      setAuth({ user, pwd, roles, accessToken });
+      setUser('');
+      setPwd('');
+      navigate(from, { replace: true });
+    } catch (err) {
+      if (!err) {
+        if (!err?.response) {
+        } else if (err.response?.status === 400) {
+          setErrMsg('Missing Username or Password');
+        } else if (err.response?.status === 401) {
+          setErrMsg('Unauthorized');
+        } else {
+          setErrMsg('Login Failed');
+        }
+        errRef.current.focus();
+      }
+    }
+  };
 
   return (
     <section>
