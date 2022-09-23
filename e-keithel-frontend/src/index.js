f44baaaf742225ba80176ab7341b947d7c7dab26
aboongm <aboongm@yahoo.com>
2022-09-23 15:50:22 +0530

    Update the index.js

--- e-keithel-frontend/src/index.js
+++ e-keithel-frontend/src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './context/AuthProvider';
+
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
