602351f18cc0800964ba7973e0c03544b44cfb0d
aboongm <aboongm@yahoo.com>
2022-09-30 19:33:09 +0530

    Created authActions.js and indexReducers.js in redux/reducers

---
+++ e-keithel-frontend/src/redux/reducers/authActions.js
@@ -0,0 +1,94 @@
+import url from '../helpers/apiUrl';
+import { AUTHENTICATED, NOT_AUTHENTICATED } from '../helpers/constants';
+
+const setToken = (token) => {
+  localStorage.setItem('token', token);
+  localStorage.setItem('lastLoginTime', new Date(Date.now()).getTime());
+};
+
+const getToken = () => {
+  const now = new Date(Date.now()).getTime();
+  const thirtyMinutes = 1000 * 60 * 30;
+  const timeSinceLastLogin = now - localStorage.getItem('lastLoginTime');
+  if (timeSinceLastLogin < thirtyMinutes) {
+    return localStorage.getItem('token');
+  }
+};
+
+export const loginUser = (credentials) => (dispatch) => {
+  // console.log('email&password: ', credentials);
+  fetch(`${url}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: credentials,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      console.log(res.headers.get('Authorization'));
+      setToken(res.headers.get('Authorization'));
+      return res
+        .json()
+        .then((userJson) =>
+          dispatch({ type: AUTHENTICATED, payload: userJson })
+        );
+    } else {
+      return res.json().then((errors) => {
+        dispatch({ type: NOT_AUTHENTICATED });
+        return Promise.reject(errors);
+      });
+    }
+  });
+};
+
+export const signupUser = (credentials) => {
+  return (dispatch) => {
+    return fetch(`${url}/signup`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: credentials }),
+    }).then((res) => {
+      if (res.ok) {
+        setToken(res.headers.get('Authorization'));
+        return res
+          .json()
+          .then((userJson) =>
+            dispatch({ type: AUTHENTICATED, payload: userJson })
+          );
+      } else {
+        return res.json().then((errors) => {
+          dispatch({ type: NOT_AUTHENTICATED });
+          return Promise.reject(errors);
+        });
+      }
+    });
+  };
+};
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    return fetch(`${url}/logout`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: getToken(),
+      },
+    }).then((res) => {
+      console.log('Logged out');
+      if (res.ok) {
+        return dispatch({ type: NOT_AUTHENTICATED });
+      } else {
+        return res.json().then((errors) => {
+          dispatch({ type: NOT_AUTHENTICATED });
+          return Promise.reject(errors);
+        });
+      }
+    });
+  };
+};
