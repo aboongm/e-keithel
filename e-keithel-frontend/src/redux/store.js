9808e0244845734ac066d91e98caf13c4c505830
aboongm <aboongm@yahoo.com>
2022-09-30 19:31:23 +0530

    Created the authReducers.js in redux/reducers

--- e-keithel-frontend/src/redux/store.js
+++ e-keithel-frontend/src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import rootReducer from './reducers/indexReducers';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    rootReducer,
+  },
   middleware: [thunk, logger],
 });
 
