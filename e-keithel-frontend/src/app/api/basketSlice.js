e2b90843cc3568c1d810b32bedd923e80d06ce5a
aboongm <aboongm@yahoo.com>
2022-10-26 17:03:59 +0530

    Add addingToBasket and removingFromBasket slices in basketSlice.js for adding and removing item functionalities

--- e-keithel-frontend/src/app/api/basketSlice.js
+++ e-keithel-frontend/src/app/api/basketSlice.js
@@ -1,15 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit" 
+import { createSlice } from '@reduxjs/toolkit';
 
 const basketSlice = createSlice({
   name: 'basket',
   initialState: { basket: [] },
   reducers: {
-    setBasket: (state, action) => {
-      state.basket = action.payload.basket
-    }
+    addingToBasket: (state, action) => {
+      state.basket = [...state.basket, action.payload];
+    },
+    removingFromBasket: (state, action) => {
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.payload
+      );
+      const newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.payload}) as its not in basket!`
+        );
+      }
+      state.basket = newBasket;
+    },
   },
-})
+});
 
-export const { setBasket } = basketSlice.actions
+export const { addingToBasket, removingFromBasket } = basketSlice.actions;
 
-export default basketSlice.reducer
+export default basketSlice.reducer;
