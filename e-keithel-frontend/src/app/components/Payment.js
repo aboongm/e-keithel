f184d709e6331ba813a127dfe7d7632b890ef5f1
aboongm <aboongm@yahoo.com>
2022-10-29 16:55:54 +0530

    Update style for Payment page for small, medium, large and xlarge screen

--- e-keithel-frontend/src/app/components/Payment.js
+++ e-keithel-frontend/src/app/components/Payment.js
@@ -24,48 +24,50 @@ const Payment = () => {
         <h1>
           Checkout (<Link to="/checkout">{basket?.length} items</Link>)
         </h1>
-        <div className="payment__section">
-          <div className="payment__title">
-            <h3>Delivery Address</h3>
+        <div className="payment__container__inner">
+          <div className="payment__section payment__order__1">
+            <div className="payment__title">
+              <h3>Delivery Address</h3>
+            </div>
+            <div className="payment__address">
+              <p>{user?.user.email}</p>
+              <p>123 React Lane</p>
+              <p>Los Angeles, CA</p>
+            </div>
           </div>
-          <div className="payment__address">
-            <p>{user?.user.email}</p>
-            <p>123 React Lane</p>
-            <p>Los Angeles, CA</p>
+          <div className="payment__section payment__order__3">
+            <div className="payment__title">
+              <h3>Review items and delivery</h3>
+            </div>
+            <div className="payment__items">
+              {basket.map((item) => (
+                <CheckoutProduct
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="payment__section">
-          <div className="payment__title">
-            <h3>Review items and delivery</h3>
-          </div>
-          <div className="payment__items">
-            {basket.map((item) => (
-              <CheckoutProduct
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                image={item.image}
-                price={item.price}
-                rating={item.rating}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="payment__section">
-          <div className="payment__title">
-            <h3>Payment Method</h3>
-          </div>
-          <div className="payment__details">
-            <form onSubmit={handleSubmit}>
-              {/* <CardElement /> */}
+          <div className="payment__section payment__order__2">
+            <div className="payment__title">
+              <h3>Payment Method</h3>
+            </div>
+            <div className="payment__details">
+              <form onSubmit={handleSubmit}>
+                {/* <CardElement /> */}
 
-              <div className="payment__priceContainer">
-                <h3>Order Total: ${getBasketTotal(basket)}</h3>
-                <button type="submit">
-                  <span>Buy Now</span>
-                </button>
-              </div>
-            </form>
+                <div className="payment__priceContainer">
+                  <h3>Order Total: ${getBasketTotal(basket)}</h3>
+                  <button type="submit">
+                    <span>Buy Now</span>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
