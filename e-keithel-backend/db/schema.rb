ed1186da93bb9f4c14d35246d30ea7b090f8c41f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-02 15:11:48 +0500

    Add Categories table

--- e-keithel-backend/db/schema.rb
+++ e-keithel-backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_090114) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_100959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.integer "code_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
