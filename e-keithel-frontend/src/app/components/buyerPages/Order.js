cd2bb44058dac577c8a0f9b8c59f38840bbc2e69
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-15 17:18:13 +0500

    Add render order

--- e-keithel-frontend/src/app/components/buyerPages/Order.js
+++ e-keithel-frontend/src/app/components/buyerPages/Order.js
@@ -1,3 +1,4 @@
+import { isError } from 'lodash';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -21,66 +22,104 @@ const Order = () => {
   //   useGetProductsQuery();
 
   // if (isLoadingProduct && isLoadingOrder) {
-  if (isLoadingOrder) {
-    <h5>Loading...</h5>;
-  }
 
-  console.log(isSuccessOrder);
-  let filterOrderList;
+  let content;
+
+  if (isLoadingOrder) content = <h5>Loading...</h5>;
+ 
+  if (isError) content = <p className="errmsg">{error?.data?.message}</p>
+  
   if (isSuccessOrder) {
-    filterOrderList = orderList.filter((order) => {
+    const filterOrderList = orderList.filter((order) => {
       return order.buyer_id === user.user.id;
     });
+
+    content = (
+      <section className="payment">
+        <div className="payment__container">
+          <div className="payment__container__inner">
+            <div className="payment__section payment__order__1">
+              <div className="payment__address">
+                <p className="text-center">{user?.user.fullname}</p>
+                <p className="text-center">{user?.user.address}</p>
+              </div>
+            </div>
+            <div className="payment__section payment__order__3">
+              <div className="payment__title">
+                <h3>Ordered items and delivery details</h3>
+              </div>
+              <div className="payment__items">
+                {orderList.map((item) => (
+                  <>
+                    <h4 key={item.id}>Order Amount: {item.amount}</h4>
+                    {item.purchase.map((product) => (
+                      <CheckoutProduct
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        image={product.image}
+                        price={product.price}
+                        rating={product.rating}
+                        orderPage={true}
+                      />
+                    ))}
+                  </>
+                ))}
+              </div>
+            </div>
+            <div className="payment__section payment__order__2">
+              <div className="payment__details"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 
   // const filterOrderList = productList.filter(({ id }) =>
   //   orderList.map(({ product_id }) => product_id).includes(id)
   // );
 
-  console.log(filterOrderList);
-
-  useEffect(() => {}, []);
-
-  const content = (
-    <section className="payment">
-      <div className="payment__container">
-        <div className="payment__container__inner">
-          <div className="payment__section payment__order__1">
-            <div className="payment__address">
-              <p className="text-center">{user?.user.fullname}</p>
-              <p className="text-center">{user?.user.address}</p>
-            </div>
-          </div>
-          <div className="payment__section payment__order__3">
-            <div className="payment__title">
-              <h3>Ordered items and delivery details</h3>
-            </div>
-            <div className="payment__items">
-              {orderList.map((item) => (
-                <>
-                  <h4 key={item.id}>Order Amount: {item.amount}</h4>
-                  {item.purchase.map((product) => (
-                    <CheckoutProduct
-                      key={product.id}
-                      id={product.id}
-                      title={product.title}
-                      image={product.image}
-                      price={product.price}
-                      rating={product.rating}
-                      orderPage={true}
-                    />
-                  ))}
-                </>
-              ))}
-            </div>
-          </div>
-          <div className="payment__section payment__order__2">
-            <div className="payment__details"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  // content = (
+  //   <section className="payment">
+  //     <div className="payment__container">
+  //       <div className="payment__container__inner">
+  //         <div className="payment__section payment__order__1">
+  //           <div className="payment__address">
+  //             <p className="text-center">{user?.user.fullname}</p>
+  //             <p className="text-center">{user?.user.address}</p>
+  //           </div>
+  //         </div>
+  //         <div className="payment__section payment__order__3">
+  //           <div className="payment__title">
+  //             <h3>Ordered items and delivery details</h3>
+  //           </div>
+  //           <div className="payment__items">
+  //             {orderList.map((item) => (
+  //               <>
+  //                 <h4 key={item.id}>Order Amount: {item.amount}</h4>
+  //                 {item.purchase.map((product) => (
+  //                   <CheckoutProduct
+  //                     key={product.id}
+  //                     id={product.id}
+  //                     title={product.title}
+  //                     image={product.image}
+  //                     price={product.price}
+  //                     rating={product.rating}
+  //                     orderPage={true}
+  //                   />
+  //                 ))}
+  //               </>
+  //             ))}
+  //           </div>
+  //         </div>
+  //         <div className="payment__section payment__order__2">
+  //           <div className="payment__details"></div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </section>
+  // );
   return content;
 };
 
