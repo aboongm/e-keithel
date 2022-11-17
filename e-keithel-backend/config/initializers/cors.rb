de7b485c15b35fb1add6c6c2a5d6720b78afd072
aboongm <aboongm@yahoo.com>
2022-11-17 11:37:16 +0530

    Update cors origins

--- e-keithel-backend/config/initializers/cors.rb
+++ e-keithel-backend/config/initializers/cors.rb
@@ -6,7 +6,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://monaaz-8zab.onrender.com/', '127.0.0.1:3001'
+    # origins 'http://localhost:3001', '127.0.0.1:3001'
+    origins 'https://monaaz-8zab.onrender.com/'
 
     resource "*",
       headers: :any,
